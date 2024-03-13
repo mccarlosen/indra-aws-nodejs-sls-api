89bdf10a5d238cc84b866db3e6fbaa6f1cb1ee06
mccarlosen <osen.10112@gmail.com>
2024-03-13 17:21:03 -0500

    Create bicycle.map.ts

---
+++ src/mappers/bicycle.map.ts
@@ -0,0 +1,17 @@
+export default {
+    "name": "nombre",
+    "height": "altura",
+    "mass": "peso",
+    "hair_color": "color_cabello",
+    "skin_color": "color_piel",
+    "eye_color": "color_ojos",
+    "birth_year": "anio_nacimiento",
+    "gender": "sexo",
+    "homeworld": "planeta_natal",
+    "films": "peliculas",
+    "species": "especies",
+    "vehicles": "vehiculos",
+    "starships": "naves_estelares",
+    "created": "fecha_creacion",
+    "edited": "fecha_modificacion"
+}
